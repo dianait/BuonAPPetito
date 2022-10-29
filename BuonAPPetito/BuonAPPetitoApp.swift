4daa122fc4295c2cc6200b658e94752f3e311331
dianait <dianahdezsoler@gmail.com>
2022-10-29 10:22:27 +0200

    Initial Commit

---
+++ BuonAPPetito/BuonAPPetitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuonAPPetitoApp.swift
+//  BuonAPPetito
+//
+//  Created by Diana Hernández on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BuonAPPetitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
