38dcc99ac238552ccc17029d2393667d9f1f1ef8
dianait <dianahdezsoler@gmail.com>
2022-11-06 17:52:23 +0100

    Add 4 ingredients as default in Pizza struct

--- BuonAPPetito/DataSource/Pizza.swift
+++ BuonAPPetito/DataSource/Pizza.swift
@@ -8,7 +8,11 @@ struct Pizza: Equatable, Identifiable, Hashable {
     let id: Int
     var name: String
     let price: Double = 9.90
-    var ingredients: [Ingredient] = []
+    var ingredients: [Ingredient] = [
+            Ingredient(id: 1, name: "🧅 Onion"),
+            Ingredient(id: 2, name: "🍅 Tomato"),
+            Ingredient(id: 3, name: "🧀 Cheese"),
+            Ingredient(id: 4, name: "🥓 Bacon")]
     var account: Int = 1
 }
 
